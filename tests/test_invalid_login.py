fea92084b2f1ecd29350788c74c8ba66e1bc646f
Prakriti <pprakriti@gmail.com>
2026-07-27 18:18:20 -0700

    Improved data driven login testing with expected results.

--- tests/test_invalid_login.py
+++ tests/test_invalid_login.py
@@ -1,22 +1,25 @@
-from pages.login_page import LoginPage
-from utils.data_reader import load_test_data
+import pytest
 
-def test_invalid_username(browser_page):
-    
+from pages.login_page import LoginPage
+from utils.data_reader import load_login_data
+
+
+@pytest.mark.parametrize(
+    "username,password,expected_error",
+    load_login_data()
+)
+def test_invalid_login(browser_page, username, password, expected_error):
+
     login_page = LoginPage(browser_page)
 
     login_page.open()
 
-    data = load_test_data()
-
     login_page.login(
-
-       data["invalid_user"]["username"],
-       data["invalid_user"]["password"]
+        username,
+        password
     )
 
     assert (
-        "Username and password do not match"
+        expected_error
         in login_page.get_error_message()
     )
-
