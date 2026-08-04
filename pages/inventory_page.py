7557bd190e59f033d5ec23bee8b4e229bcbb70a9
Prakriti <pprakriti@gmail.com>
2026-08-04 11:12:32 -0700

    Add inventory cart test and improve CI reporting

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -8,6 +8,8 @@ class InventoryPage:
     def __init__(self, page):
         self.page = page
         self.title = ".title"
+        self.add_to_cart_button = "#add-to-cart-sauce-labs-backpack"
+        self.cart_badge = ".shopping_cart_badge"
 
 
     def is_loaded(self):
@@ -35,3 +37,23 @@ class InventoryPage:
             )
 
             return False
+    def add_product_to_cart(self):
+
+        self.logger.info(
+            "Adding Sauce Labs Backpack to cart"
+        )
+
+        self.page.locator(
+            self.add_to_cart_button
+        ).click()
+
+
+    def get_cart_count(self):
+
+        self.logger.info(
+            "Getting cart item count"
+        )
+
+        return self.page.locator(
+            self.cart_badge
+        ).inner_text()
---
+++ tests/test_inventory.py
@@ -0,0 +1,24 @@
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+
+
+def test_add_product_to_cart(browser_page):
+
+    login_page = LoginPage(browser_page)
+
+    login_page.open()
+
+    login_page.login(
+        "standard_user",
+        "secret_sauce"
+    )
+
+    inventory_page = InventoryPage(
+        browser_page
+    )
+
+    assert inventory_page.is_loaded()
+
+    inventory_page.add_product_to_cart()
+
+    assert inventory_page.get_cart_count() == "1"
